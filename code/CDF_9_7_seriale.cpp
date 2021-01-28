8970c7ecf93907cc4a304bd627e064eca8c5735d
Giuseppe Carniglia <giuseppe.carniglia01@universitadipavia.it>
2021-01-28 10:34:15 +0100

    Added comments to code. All .cpp file and MAkefile are in directory code

---
+++ code/CDF_9_7_seriale.cpp
@@ -0,0 +1,293 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <math.h>
+#include <omp.h>
+
+using namespace cv;
+using namespace std;
+
+#define ZERO_VALUE (uchar)0
+
+/*
+   
+   
+   ________________________________________________9/7-CDF Transform_________________________________________________________
+   * 
+   * 
+  
+ */
+
+
+int main(int argc, char *argv[])	
+{
+	const char* imgName = argv[1];
+	Mat image = imread(imgName, IMREAD_GRAYSCALE);
+	int imageWidth = image.size().width;
+	int imageHeight = image.size().height;
+	Mat new_image = cv::Mat(imageHeight,imageWidth,CV_8UC1);
+
+	int w = imageWidth;
+
+	double coefficientsLP[5] = { 0.602949018236,0.266864118443,-0.078223266529,-0.016864118443,0.26748757411 };
+	double coefficientsHP[5] = { 1.11508705, -0.59127176314, -0.057543526229, 0.091271763114, 0.0 };
+	int padding[3] = { (w / 2) - 2 ,(w / 2) - 1 ,(w / 2) };
+	double initial_time = 0, final_time = 0;
+	int i=0,j=0;
+
+	double time_A=0,time_D=0,time_AA=0,time_DD=0;
+	initial_time = omp_get_wtime();
+
+	
+	time_A = omp_get_wtime();
+	
+	//Calculations for matrix A
+	for (i = 0; i < imageHeight; i++) {
+	
+		new_image.at<uchar>(i,0) = coefficientsLP[0] * image.at<uchar>(i,0) 
+			+ coefficientsLP[1] * (image.at<uchar>(i, 1) + ZERO_VALUE)
+			+ coefficientsLP[2] * (image.at<uchar>(i, 2) + ZERO_VALUE)
+			+ coefficientsLP[3] * (image.at<uchar>(i, 3) + ZERO_VALUE)
+			+ coefficientsLP[4] * (image.at<uchar>(i, 4) + ZERO_VALUE);
+
+		new_image.at<uchar>(i,1) = coefficientsLP[0] * image.at<uchar>(i, 2) 
+			+ coefficientsLP[1] * (image.at<uchar>(i, 3) + image.at<uchar>(i, 1))
+			+ coefficientsLP[2] * (image.at<uchar>(i, 4) + image.at<uchar>(i, 0))
+			+ coefficientsLP[3] * (image.at<uchar>(i, 5) + ZERO_VALUE)
+			+ coefficientsLP[4] * (image.at<uchar>(i, 6) + ZERO_VALUE);
+
+		new_image.at<uchar>(i,2) = coefficientsLP[0] * image.at<uchar>(i, 4) 
+			+ coefficientsLP[1] * (image.at<uchar>(i, 5) + image.at<uchar>(i, 3))
+			+ coefficientsLP[2] * (image.at<uchar>(i, 6) + image.at<uchar>(i, 2))
+			+ coefficientsLP[3] * (image.at<uchar>(i, 7) + image.at<uchar>(i, 1))
+			+ coefficientsLP[4] * (image.at<uchar>(i, 8) + image.at<uchar>(i, 0));
+
+
+		for (j = 3; j < (w/2) - 2; j++) {
+			new_image.at<uchar>(i,j) = coefficientsLP[0] * image.at<uchar>(i, (j<<1))
+				+ coefficientsLP[1] * (image.at<uchar>(i, (j<<1) + 1) + image.at<uchar>(i, (j<<1) - 1))
+				+ coefficientsLP[2] * (image.at<uchar>(i, (j<<1) + 2) + image.at<uchar>(i, (j<<1) - 2))
+				+ coefficientsLP[3] * (image.at<uchar>(i, (j<<1) + 3) + image.at<uchar>(i, (j<<1) - 3))
+				+ coefficientsLP[4] * (image.at<uchar>(i, (j<<1) + 4) + image.at<uchar>(i, (j<<1) - 4));
+		}
+
+		new_image.at<uchar>(i,padding[0]) = coefficientsLP[0] * image.at<uchar>(i,(padding[0]<<1))
+			+ coefficientsLP[1] * (image.at<uchar>(i, (padding[0]<<1) + 1) 
+			+ image.at<uchar>(i, (padding[0]<<1) - 1))
+			+ coefficientsLP[2] * (image.at<uchar>(i, (padding[0]<<1) + 2) 
+			+ image.at<uchar>(i, (padding[0]<<1) - 2))
+			+ coefficientsLP[3] * (image.at<uchar>(i, (padding[0]<<1) + 3) 
+			+ image.at<uchar>(i, (padding[0]<<1) - 3))
+			+ coefficientsLP[4] * (ZERO_VALUE + image.at<uchar>(i, (padding[0]<<1) - 4));
+
+		new_image.at<uchar>(i,padding[1]) = coefficientsLP[0] * image.at<uchar>(i, (padding[1]<<1))
+			+ coefficientsLP[1] * (image.at<uchar>(i,(padding[1]<<1) + 1) 
+			+ image.at<uchar>(i, (padding[1]<<1) - 1))
+			+ coefficientsLP[2] * (ZERO_VALUE + image.at<uchar>(i, (padding[1]<<1) - 2))
+			+ coefficientsLP[3] * (ZERO_VALUE + image.at<uchar>(i, (padding[1]<<1) - 3))
+			+ coefficientsLP[4] * (ZERO_VALUE + image.at<uchar>(i, (padding[1]<<1) - 4));
+
+		new_image.at<uchar>(i,padding[2]) = ZERO_VALUE
+			+ coefficientsLP[1] * (ZERO_VALUE + image.at<uchar>(i, (padding[2] << 1) - 1))
+			+ coefficientsLP[2] * (ZERO_VALUE + image.at<uchar>(i, (padding[2] << 1) - 2))
+			+ coefficientsLP[3] * (ZERO_VALUE + image.at<uchar>(i, (padding[2] << 1) - 3))
+			+ coefficientsLP[4] * (ZERO_VALUE + image.at<uchar>(i, (padding[2] << 1) - 4));
+	}
+
+	//Calculations for matrix D
+	for (i = 0; i < image.rows ; i++) {
+	
+		new_image.at<uchar>(i,0+(w/2)) = coefficientsHP[0] * image.at<uchar>(i, 0)
+			+ coefficientsHP[1] * (image.at<uchar>(i, 1) + ZERO_VALUE)
+			+ coefficientsHP[2] * (image.at<uchar>(i, 2) + ZERO_VALUE)
+			+ coefficientsHP[3] * (image.at<uchar>(i, 3) + ZERO_VALUE)
+			+ coefficientsHP[4] * (image.at<uchar>(i, 4) + ZERO_VALUE);
+
+		new_image.at<uchar>(i,1+(w/2)) = coefficientsHP[0] * image.at<uchar>(i, 2)
+			+ coefficientsHP[1] * (image.at<uchar>(i, 3) + image.at<uchar>(i, 1))
+			+ coefficientsHP[2] * (image.at<uchar>(i, 4) + image.at<uchar>(i, 0))
+			+ coefficientsHP[3] * (image.at<uchar>(i, 5) + ZERO_VALUE)
+			+ coefficientsHP[4] * (image.at<uchar>(i, 6) + ZERO_VALUE);
+
+		new_image.at<uchar>(i,2+(w/2)) = coefficientsHP[0] * image.at<uchar>(i, 4)
+			+ coefficientsHP[1] * (image.at<uchar>(i, 5) + image.at<uchar>(i, 3))
+			+ coefficientsHP[2] * (image.at<uchar>(i, 6) + image.at<uchar>(i, 2))
+			+ coefficientsHP[3] * (image.at<uchar>(i, 7) + image.at<uchar>(i, 1))
+			+ coefficientsHP[4] * (image.at<uchar>(i, 8) + image.at<uchar>(i, 0));
+
+		
+		for (j = 3; j < (imageWidth/2) - 2; j++) {
+			 new_image.at<uchar>(i,j+(w/2)) = coefficientsHP[0] * image.at<uchar>(i, (j<<1))
+				+ coefficientsHP[1] * (image.at<uchar>(i, (j<<1) + 1)+image.at<uchar>(i, (j<<1) - 1))
+				+ coefficientsHP[2] * (image.at<uchar>(i, (j<<1) + 2)+image.at<uchar>(i, (j<<1) - 2))
+				+ coefficientsHP[3] * (image.at<uchar>(i, (j<<1) + 3)+image.at<uchar>(i, (j<<1) - 3))
+				+ coefficientsHP[4] * (image.at<uchar>(i, (j<<1) + 4)+image.at<uchar>(i, (j<<1) - 4));
+		}
+		
+		new_image.at<uchar>(i,padding[0]+(w/2)) = coefficientsHP[0] * image.at<uchar>(i, (padding[0] << 1))
+			+ coefficientsHP[1] * (image.at<uchar>(i, (padding[0] << 1) + 1) 
+				+ image.at<uchar>(i, (padding[0] << 1) - 1))
+			+ coefficientsHP[2] * (image.at<uchar>(i, (padding[0] << 1) + 2) 
+				+ image.at<uchar>(i, (padding[0] << 1) - 2))
+			+ coefficientsHP[3] * (image.at<uchar>(i, (padding[0] << 1) + 3) 
+				+ image.at<uchar>(i, (padding[0] << 1) - 3))
+			+ coefficientsHP[4] * (ZERO_VALUE + image.at<uchar>(i, (padding[0] << 1) - 4));
+		
+		new_image.at<uchar>(i,padding[1]+(w/2)) = coefficientsHP[0] * image.at<uchar>(i, (padding[1] << 1))
+			+ coefficientsHP[1] * (image.at<uchar>(i, (padding[1]<<1)+1) + image.at<uchar>(i, (padding[1] << 1) - 1))
+			+ coefficientsHP[2] * (ZERO_VALUE + image.at<uchar>(i, (padding[1] << 1) - 2))
+			+ coefficientsHP[3] * (ZERO_VALUE + image.at<uchar>(i, (padding[1] << 1) - 3))
+			+ coefficientsHP[4] * (ZERO_VALUE + image.at<uchar>(i, (padding[1] << 1) - 4));
+		
+		new_image.at<uchar>(i,padding[2]+(w/2)) = ZERO_VALUE 
+			+ coefficientsHP[1] * (ZERO_VALUE + image.at<uchar>(i, (padding[2]<<1) - 1))
+			+ coefficientsHP[2] * (ZERO_VALUE + image.at<uchar>(i, (padding[2]<<1) - 2))
+			+ coefficientsHP[3] * (ZERO_VALUE + image.at<uchar>(i, (padding[2]<<1) - 3))
+			+ coefficientsHP[4] * (ZERO_VALUE + image.at<uchar>(i, (padding[2]<<1) - 4));
+	}
+
+	time_D = omp_get_wtime();
+	time_D -= time_A;
+
+
+	
+	image = new_image.clone();
+
+	w = imageHeight;
+	padding[0] = (w/2)-2;
+	padding[1] = (w/2)-1;
+	padding[2] = (w/2);
+	
+	time_AA =  omp_get_wtime();
+	
+	//Calculations for matrices AA and DA
+	for (i = 0; i < imageWidth; i++) {
+		
+		new_image.at<uchar>(0, i) = coefficientsLP[0] * image.at<uchar>(0,i) 
+			+ coefficientsLP[1] * (image.at<uchar>(1,i) + ZERO_VALUE)
+			+ coefficientsLP[2] * (image.at<uchar>(2,i) + ZERO_VALUE)
+			+ coefficientsLP[3] * (image.at<uchar>(3,i) + ZERO_VALUE)
+			+ coefficientsLP[4] * (image.at<uchar>(4,i) + ZERO_VALUE);
+
+		new_image.at<uchar>(1, i) = coefficientsLP[0] * image.at<uchar>(2,i) 
+			+ coefficientsLP[1] * (image.at<uchar>(3,i) + image.at<uchar>(1,i))
+			+ coefficientsLP[2] * (image.at<uchar>(4,i) + image.at<uchar>(0,i))
+			+ coefficientsLP[3] * (image.at<uchar>(5,i) + ZERO_VALUE)
+			+ coefficientsLP[4] * (image.at<uchar>(6,i) + ZERO_VALUE);
+
+		new_image.at<uchar>(2, i) = coefficientsLP[0] * image.at<uchar>(4,i) 
+			+ coefficientsLP[1] * (image.at<uchar>(5,i) + image.at<uchar>(3,i))
+			+ coefficientsLP[2] * (image.at<uchar>(6,i) + image.at<uchar>(2,i))
+			+ coefficientsLP[3] * (image.at<uchar>(7,i) + image.at<uchar>(1,i))
+			+ coefficientsLP[4] * (image.at<uchar>(8,i) + image.at<uchar>(0,i));
+
+
+		for (j = 3; j < (w/2) - 2; j++) {
+			new_image.at<uchar>(j, i) = coefficientsLP[0] * image.at<uchar>((j<<1),i)
+				+ coefficientsLP[1] * (image.at<uchar>((j<<1) + 1,i) + image.at<uchar>((j<<1) - 1,i))
+				+ coefficientsLP[2] * (image.at<uchar>((j<<1) + 2,i) + image.at<uchar>((j<<1) - 2,i))
+				+ coefficientsLP[3] * (image.at<uchar>((j<<1) + 3,i) + image.at<uchar>((j<<1) - 3,i))
+				+ coefficientsLP[4] * (image.at<uchar>((j<<1) + 4,i) + image.at<uchar>((j<<1) - 4,i));
+		}
+
+		new_image.at<uchar>(padding[0], i) = coefficientsLP[0] * image.at<uchar>((padding[0]<<1),i)
+			+ coefficientsLP[1] * (image.at<uchar>((padding[0]<<1) + 1,i) 
+			+ image.at<uchar>((padding[0]<<1) - 1,i))
+			+ coefficientsLP[2] * (image.at<uchar>((padding[0]<<1) + 2,i) 
+			+ image.at<uchar>((padding[0]<<1) - 2,i))
+			+ coefficientsLP[3] * (image.at<uchar>((padding[0]<<1) + 3,i) 
+			+ image.at<uchar>((padding[0]<<1) - 3,i))
+			+ coefficientsLP[4] * (ZERO_VALUE + image.at<uchar>((padding[0]<<1) - 4,i));
+
+		new_image.at<uchar>(padding[1], i) = coefficientsLP[0] * image.at<uchar>((padding[1]<<1),i)
+			+ coefficientsLP[1] * (image.at<uchar>((padding[1]<<1) + 1,i) 
+			+ image.at<uchar>((padding[1]<<1) - 1,i))
+			+ coefficientsLP[2] * (ZERO_VALUE + image.at<uchar>((padding[1]<<1) - 2,i))
+			+ coefficientsLP[3] * (ZERO_VALUE + image.at<uchar>((padding[1]<<1) - 3,i))
+			+ coefficientsLP[4] * (ZERO_VALUE + image.at<uchar>((padding[1]<<1) - 4,i));
+
+		new_image.at<uchar>(padding[2], i) = ZERO_VALUE
+			+ coefficientsLP[1] * (ZERO_VALUE + image.at<uchar>((padding[2]<<1) - 1,i))
+			+ coefficientsLP[2] * (ZERO_VALUE + image.at<uchar>((padding[2]<<1) - 2,i))
+			+ coefficientsLP[3] * (ZERO_VALUE + image.at<uchar>((padding[2]<<1) - 3,i))
+			+ coefficientsLP[4] * (ZERO_VALUE + image.at<uchar>((padding[2]<<1) - 4,i));
+	}
+
+	//Calculations for matrices DA and DD
+	for (i = 0; i < imageWidth ; i++) {
+
+		new_image.at<uchar>(0+(w/2 - 1),i) = coefficientsHP[0] * new_image.at<uchar>(0,i)
+			+ coefficientsHP[1] * (image.at<uchar>(1,i) + ZERO_VALUE)
+			+ coefficientsHP[2] * (image.at<uchar>(2,i) + ZERO_VALUE)
+			+ coefficientsHP[3] * (image.at<uchar>(3,i) + ZERO_VALUE)
+			+ coefficientsHP[4] * (image.at<uchar>(4,i) + ZERO_VALUE);
+
+		new_image.at<uchar>(1+(w/2 - 1),i) = coefficientsHP[0] * image.at<uchar>(2,i)
+			+ coefficientsHP[1] * (image.at<uchar>(3,i) + image.at<uchar>(1,i))
+			+ coefficientsHP[2] * (image.at<uchar>(4,i) + image.at<uchar>(0,i))
+			+ coefficientsHP[3] * (image.at<uchar>(5,i) + ZERO_VALUE)
+			+ coefficientsHP[4] * (image.at<uchar>(6,i) + ZERO_VALUE);
+
+		new_image.at<uchar>(2+(w/2 - 1),i) = coefficientsHP[0] * image.at<uchar>(4,i)
+			+ coefficientsHP[1] * (image.at<uchar>(5,i) + image.at<uchar>(3,i))
+			+ coefficientsHP[2] * (image.at<uchar>(6,i) + image.at<uchar>(2,i))
+			+ coefficientsHP[3] * (image.at<uchar>(7,i) + image.at<uchar>(1,i))
+			+ coefficientsHP[4] * (image.at<uchar>(8,i) + image.at<uchar>(0,i));
+
+		
+		for (j = 3; j < (w/2) - 2; j++) {
+			new_image.at<uchar>(j+(w/2 - 1),i) = coefficientsHP[0] * image.at<uchar>((j<<1),i)
+				+ coefficientsHP[1] * (image.at<uchar>((j<<1) + 1,i)+image.at<uchar>((j<<1) - 1,i))
+				+ coefficientsHP[2] * (image.at<uchar>((j<<1) + 2,i)+image.at<uchar>((j<<1) - 2,i))
+				+ coefficientsHP[3] * (image.at<uchar>((j<<1) + 3,i)+image.at<uchar>((j<<1) - 3,i))
+				+ coefficientsHP[4] * (image.at<uchar>((j<<1) + 4,i)+image.at<uchar>((j<<1) - 4,i));
+		}
+		
+		new_image.at<uchar>(padding[0]+(w/2 - 1),i) = coefficientsHP[0] * image.at<uchar>((padding[0]<<1),i)
+			+ coefficientsHP[1] * (image.at<uchar>((padding[0]<<1)+1,i) + image.at<uchar>((padding[0]<<1)-1,i))
+			+ coefficientsHP[2] * (image.at<uchar>((padding[0]<<1)+2,i) + image.at<uchar>((padding[0]<<1)-2,i))
+			+ coefficientsHP[3] * (image.at<uchar>((padding[0]<<1)+3,i) + image.at<uchar>((padding[0]<<1)-3,i))
+			+ coefficientsHP[4] * (ZERO_VALUE + image.at<uchar>((padding[0]<<1)-4,i));
+		
+		new_image.at<uchar>(padding[1]+(w/2 - 1),i) = coefficientsHP[0] * image.at<uchar>((padding[1]<<1),i)
+			+ coefficientsHP[1] * (image.at<uchar>((padding[1]<<1)+1,i) + image.at<uchar>((padding[1]<<1)-1,i))
+			+ coefficientsHP[2] * (image.at<uchar>((padding[1]<<1)+2,i) + image.at<uchar>((padding[1]<<1)-2,i))
+			+ coefficientsHP[3] * (ZERO_VALUE + image.at<uchar>((padding[1]<<1)-3,i))
+			+ coefficientsHP[4] * (ZERO_VALUE + image.at<uchar>((padding[1]<<1)-4,i));
+
+
+		new_image.at<uchar>(padding[2]+(w/2 - 1),i) = ZERO_VALUE
+			+ coefficientsHP[1] * (ZERO_VALUE + image.at<uchar>((padding[2]<<1)-1,i))
+			+ coefficientsHP[2] * (ZERO_VALUE + image.at<uchar>((padding[2]<<1)-2,i))
+			+ coefficientsHP[3] * (ZERO_VALUE + image.at<uchar>((padding[2]<<1)-3,i))
+			+ coefficientsHP[4] * (ZERO_VALUE + image.at<uchar>((padding[2]<<1)-4,i));
+		
+	}
+	time_DD = omp_get_wtime();
+	time_DD -= time_AA;
+	final_time = omp_get_wtime();
+	final_time -= initial_time;
+
+	printf("time %lf\n", final_time);
+	printf("time A e D %lf\n", time_D);
+	printf("time AA e DD %lf\n", time_DD);
+	
+	imshow("Serial CDDF 9/7",new_image);
+	waitKey(0);
+
+//Functions to save image on disk
+//	vector<int> compression_params;
+
+//	compression_params.push_back(cv::IMWRITE_JPEG_QUALITY);
+
+//	compression_params.push_back(60);
+
+//	imwrite("./immagini_modificate/CDF_9_7_seriale/CDF_9_7_seriale.jpg",new_image,compression_params);
+
+	return 0;
+}
+
+
+
+
---
+++ code/Haar_seriale.cpp
@@ -0,0 +1,185 @@
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+#include <math.h>
+#include<omp.h>
+
+using namespace cv;
+using namespace std;
+
+#define ZERO_VALUE (uchar)0
+
+
+
+/*
+________________________________________________Haar Transform_________________________________________________________
+*/
+
+Mat Haar_antitrasformata(Mat immagine_trasformata);
+
+
+Mat diff_of_images(Mat original, Mat antitransform);
+
+
+int main(int argc, char *argv[]) {
+	const char* imgName = argv[1];
+	Mat image_original = imread(imgName, IMREAD_GRAYSCALE);
+	Mat image = imread(imgName, IMREAD_GRAYSCALE);
+
+	Mat dst = cv::Mat(image.rows,image.cols,CV_8UC1);
+
+	Mat antitransform = cv::Mat(image.rows,image.cols,CV_8UC1);
+	Mat image_difference = cv::Mat(image.rows,image.cols,CV_8UC1);
+		
+	int w = 0;
+
+	double initial_time = 0, final_time = 0;
+	int temp_row = 0;
+	int temp_col = 0;
+
+
+	int i=0;
+	int j=0;
+
+	temp_row = (image.rows);
+	temp_col = (image.cols);
+
+	double time_A = 0;
+	double time_D = 0;
+	double time_AA = 0;
+	double time_DD = 0;
+
+	initial_time = omp_get_wtime();
+
+	time_A = omp_get_wtime();
+	//immagine A e D
+	for (i = 0; i < temp_row; i++)
+	{
+		w = (image.cols);
+
+		for (j = 0; j < w / 2; j++) {
+			dst.at<uchar>(i,j) = (image.at<uchar>(i, j + j) + image.at<uchar>(i, j + j + 1)) / 2;
+		}
+		for (j = 0; j < w/2; j++) {
+			dst.at<uchar>(i,j+(w/2)) = (image.at<uchar>(i, j +j) - image.at<uchar>(i, j+j+1))/2;
+		}
+
+	}
+
+	//Redundant code is possible to eliminate but would change execution times so for 
+	//consistency we kept it
+	for (int i = 0; i < temp_row; i++) {
+		for (int j = 0; j < temp_col; j++) {
+			image.at<uchar>(i, j) = dst.at<uchar>(i, j);
+		}
+	}
+
+	time_D = omp_get_wtime();
+	time_D -= time_A; // execution time of first calculation
+
+	w = (image.rows);
+        
+        time_AA = omp_get_wtime();
+		for (int j = 0; j < w / 2; j++) {
+
+        for(int i=0;i<image.cols;i++){
+		dst.at<uchar>(j,i) = (image.at<uchar>(2*j, i) + image.at<uchar>(2*j+1, i)) / 2;
+
+		dst.at<uchar>(j+(w/2),i) = (image.at<uchar>(2*j, i) - image.at<uchar>(2*j+1, i)) / 2;
+		}
+        }
+        time_DD = omp_get_wtime();
+        time_DD -= time_AA; // execution time of second calculation
+        
+	final_time = omp_get_wtime();
+	final_time -= initial_time;
+	printf("time: %lf \n", final_time);
+
+	printf("time immagini A e D: %lf \n", time_D);
+
+    
+	printf("time immagini AA e DA: %lf \n", time_DD);
+	
+	imshow("Haar transform serial",dst);
+	
+//	antitransform = Haar_antitrasformata(image);
+//	image_difference = diff_of_images(image_original, antitransform);
+
+//	double min, max;
+//	cv::minMaxLoc(image_difference, &min, &max);
+
+//	printf("Image difference\nLowest value:%lf Highest value:%lf\n",min,max);
+
+//Functions to save images on disk
+/*	vector<int> compression_params;
+
+	compression_params.push_back(cv::IMWRITE_JPEG_QUALITY);
+
+	compression_params.push_back(60);
+
+	imwrite("./immagini_modificate/Haar_seriale/Haar_seriale.jpg",image,compression_params);
+
+	imwrite("./immagini_modificate/Haar_seriale/Haar_seriale_antitrasformata.jpg",antitransform,compression_params);
+	imwrite("./immagini_modificate/Haar_seriale/Haar_seriale_differenza.jpg",image_difference,compression_params);
+	
+	imshow("Haar antitransform serial",image_difference);*/
+
+
+	waitKey(0);
+
+	return 0;
+}
+
+// Haar antitransform
+Mat Haar_antitrasformata(Mat immagine_trasformata){
+
+	Mat image_out = cv::Mat(immagine_trasformata.rows, immagine_trasformata.cols, CV_8UC1);
+	Mat image_out_2 = cv::Mat(immagine_trasformata.rows, immagine_trasformata.cols, CV_8UC1);
+
+	int k = immagine_trasformata.rows;
+	int w = immagine_trasformata.cols;
+	int i=0, j=0;
+
+	for(i=0;i<k/2;i++){
+		for(j=0;j<w;j++){
+			image_out.at<uchar>(2*i,j) = immagine_trasformata.at<uchar>(i,j) + immagine_trasformata.at<uchar>(i+(k/2),j);
+
+			image_out.at<uchar>(2*i+1,j) = immagine_trasformata.at<uchar>(i,j) - immagine_trasformata.at<uchar>(i+(k/2),j);
+
+		}
+	}
+
+	for(i=0;i<k;i++){
+		for(j=0;j<w/2;j++){
+			image_out_2.at<uchar>(i,2*j) = image_out.at<uchar>(i,j) + image_out.at<uchar>(i,j+(w/2));
+
+			image_out_2.at<uchar>(i,2*j+1) = image_out.at<uchar>(i,j) - image_out.at<uchar>(i,j+(w/2));
+
+		}
+	}
+
+	return image_out_2;
+}
+
+//Difference of images
+Mat diff_of_images(Mat original, Mat antitransform){
+
+	if(original.cols != antitransform.cols || original.rows != antitransform.rows){
+		perror("Images of different sizes");
+	}
+
+	Mat image_diff = cv::Mat(original.rows, original.cols, CV_8UC1);
+
+	int i=0, j=0;
+
+	for(i=0;i<original.rows;i++){
+
+		for(j=0;j<original.cols;j++){
+
+			image_diff.at<uchar>(i,j) = original.at<uchar>(i,j) - antitransform.at<uchar>(i,j);
+		}
+	}
+
+	return image_diff;
+}
